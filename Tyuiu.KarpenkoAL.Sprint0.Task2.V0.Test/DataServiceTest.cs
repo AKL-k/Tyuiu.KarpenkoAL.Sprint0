a08136b62b40dccc790ed05201e5d2e711e07c56
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-09 16:16:27 +0500

    Task2

---
+++ Tyuiu.KarpenkoAL.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KarpenkoAL.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KarpenkoAL.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Артём";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Артём", res);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.KarpenkoAL.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Артём"));
+Console.ReadKey();
+
